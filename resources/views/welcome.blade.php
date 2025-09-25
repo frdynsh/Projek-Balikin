b91cf43dd48b788cd7993053156c5f05fceb772c
Ferdi Yansah <frdynsh11@gmail.com>
2025-09-25 17:14:08 +0700

    feat:fix repair found item module, lost item module and welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                         <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">Jangan panik. "Balikin" membantumu menemukan kembali barang berhargamu dan melaporkan barang yang kamu temukan. Platform Lost & Found untuk seluruh warga kampus.</p>
                         <div class="mt-10 flex items-center justify-center gap-x-6">
                             <a href="{{ route('register') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Gabung Sekarang</a>
-                            <a href="{{ route('lostitems.index') }}" class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">Lihat Barang Hilang <span aria-hidden="true">→</span></a>
+                            <a href="{{ route('lost-items.index') }}" class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">Lihat Barang Hilang <span aria-hidden="true">→</span></a>
                         </div>
                     </div>
                 </div>
